bfed0ac301939cf38904f48fe666d0953b3d78e0
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2025-06-28 14:42:15 +0300

    Lab6: main.rs

--- lab6/src/main.rs
+++ lab6/src/main.rs
@@ -3,16 +3,10 @@ mod substring;
 use substring::{boyer_moore, kmp, rabin_karp};
 
 fn main() {
-    let text = "ABABAC";
-    let pattern = "ABA";
+    let text = "ABACABABACB";
+    let pattern = "ABAC";
 
     println!("Rabin-Karp: {:?}", rabin_karp(text, pattern));
-    println!("KMP: {:?}", kmp(text, pattern));
     println!("Boyer-Moore: {:?}", boyer_moore(text, pattern));
-
-    // Вопросы:
-    // 1. Эвристика
-    // 2. Стоп символ хорошего суф
-    // 3. Как работатет без таблицы перехода
-    // 4. КМП: алг префикса, функ без табл (изначальная логика)
+    println!("KMP: {:?}", kmp(text, pattern));
 }
