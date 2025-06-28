a99995b70c3b91c7726678c4f319dd2dd86b028b
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2025-06-28 11:06:43 +0300

    Lab 6: rabin_karp  as stated in task. + Inglish

--- lab6/src/substring.rs
+++ lab6/src/substring.rs
@@ -1,42 +1,57 @@
+/*
+d — размер алфавита
+q — некоторое простое
+Rabin-Karp(T,P)
+    n = len(T)
+    m = len(P)
+    d_m = d^(m-1) mod q
+    h = h' = 0
+    for i = 0 to m-1
+        h = d*h + P[i] mod q
+        h'= d*h'+ T[i] mod q
+    for s = 0 to n-m
+        if h == h' then
+            if P == T[s..s+m-1] then
+                s → result
+            if s < n-m then
+                h'= (d*(h'-d_m*T[s])+T[s+m]) mod q
+*/
 pub fn rabin_karp(text: &str, pattern: &str) -> Vec<usize> {
-    const BASE: u64 = 256;
-    const MOD: u64 = 1_000_000_000;
-
+    let d: u64 = 256; // ASCII
+    let q: u64 = 107; // Prime number
     let n = text.len();
     let m = pattern.len();
+
     let mut result = Vec::new();
 
-    if pattern.is_empty() || m > n {
+    if m == 0 || m > n {
         return result;
     }
 
     let text_bytes = text.as_bytes();
     let pattern_bytes = pattern.as_bytes();
 
-    let mut hash_pattern = 0u64;
-    let mut hash_text = 0u64;
-    let mut h = 1u64;
-
+    let mut d_m = 1u64;
     for _ in 0..m - 1 {
-        h = (h * BASE) % MOD;
+        d_m = (d_m * d) % q; // d^(m-1) mod q
     }
+
+    let mut h = 0u64; // P hash
+    let mut h_t = 0u64; // T hash
 
     for i in 0..m {
-        hash_pattern = (BASE * hash_pattern + pattern_bytes[i] as u64) % MOD;
-        hash_text = (BASE * hash_text + text_bytes[i] as u64) % MOD;
+        h = (d * h + pattern_bytes[i] as u64) % q;
+        h_t = (d * h_t + text_bytes[i] as u64) % q;
     }
 
-    for i in 0..=n - m {
-        if hash_pattern == hash_text {
-            if &text[i..i + m] == pattern {
-                result.push(i);
+    for s in 0..=n - m {
+        if h == h_t {
+            if &text[s..s + m] == pattern {
+                result.push(s);
             }
         }
-
-        if i < n - m {
-            hash_text = (BASE * (hash_text + MOD - text_bytes[i] as u64 * h % MOD)
-                + text_bytes[i + m] as u64)
-                % MOD;
+        if s < n - m {
+            h_t = (d * (h_t + q - (text_bytes[s] as u64 * d_m) % q) + text_bytes[s + m] as u64) % q;
         }
     }
 
@@ -52,7 +67,7 @@ pub fn kmp(text: &str, pattern: &str) -> Vec<usize> {
         return result;
     }
 
-    // Префикс-функция
+    // Prefix function
     let mut lps = vec![0; pattern.len()];
     let mut len = 0;
     for i in 1..pattern.len() {
@@ -65,7 +80,7 @@ pub fn kmp(text: &str, pattern: &str) -> Vec<usize> {
         }
     }
 
-    // Поиск
+    // Search
     let mut i = 0;
     let mut j = 0;
     while i < text.len() {
